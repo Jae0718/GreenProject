a2fa4ee3d6dc501530571bb33748cfff2a0c30bf
hamu3 <hamu3@DESKTOP-5HQI5BN>
2019-10-30 17:43:14 +0900

    GitTest - 클래스 생성

---
+++ GitTest/src/TestClass.java
@@ -0,0 +1,27 @@
+
+public class TestClass {
+	private int a;
+	
+	public TestClass() {
+		
+	}
+
+	
+	public int getA() {
+		return a;
+	}
+
+
+	public void setA(int a) {
+		this.a = a;
+	}
+
+
+	@Override
+	public String toString() {
+		return super.toString();
+	}
+	
+	
+	
+}
